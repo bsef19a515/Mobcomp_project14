eb0e9d929f20d75bc165fc28d9b4232642c15cca
kinza <bsef19a515@pucit.edu.pk>
2022-08-15 23:04:00 +0500

    java classes add

---
+++ app/src/main/java/com/example/myapplication13/StudentModel.java
@@ -0,0 +1,46 @@
+package com.example.myapplication13;
+
+public class StudentModel {
+    private String name;
+    private int rollNumber;
+    private boolean isEnroll;
+
+    @Override
+    public String toString() {
+        return "StudentModel{" +
+                "name='" + name + '\'' +
+                ", rollNmber=" + rollNumber +
+                ", isEnroll=" + isEnroll +
+                '}';
+    }
+
+    public StudentModel(String name, int rollNumber, boolean isEnroll) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.isEnroll = isEnroll;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNmber() {
+        return rollNumber;
+    }
+
+    public void setRollNmber(int rollNmber) {
+        this.rollNumber = rollNmber;
+    }
+
+    public boolean isEnroll() {
+        return isEnroll;
+    }
+
+    public void setEnroll(boolean enroll) {
+        isEnroll = enroll;
+    }
+}
